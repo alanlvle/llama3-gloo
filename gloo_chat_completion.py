099fbf0af3ec70e4fd9c1202ed887f620dc80a00
lyh <252491900@qq.com>
2024-05-12 21:28:51 +0800

    add cmdb train

--- gloo_chat_completion.py
+++ gloo_chat_completion.py
@@ -1,3 +1,5 @@
+import datetime
+
 from llama import Dialog, Llama
 import os
 import torch
@@ -11,6 +13,7 @@ print(torch.cuda.is_available())
 print(torch.backends.cudnn.is_available())
 print(torch.cuda_version)
 print(torch.backends.cudnn.version())
+print(datetime.datetime.utcnow())
 
 os.environ['MASTER_ADDR'] = 'localhost'
 os.environ['MASTER_PORT'] = '12355'
@@ -18,27 +21,49 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 os.environ["RANK"] = "0"
 os.environ["WORLD_SIZE"] = "1"
 
+check_point_directory = "Meta-Llama-3-8B-Instruct"
+tokenizer_path = "Meta-Llama-3-8B-Instruct/tokenizer.model"
+temperature = 0.6  # How much randomness is included in model output
+top_p = 0.9  # Top-p probability threshold for nucleus sampling
+max_sequence_length = 512  # The llama 3 model model produces up to 8192 tokens so max sequence length needs to be <= 8192
+max_batch_size = 4
+model_parallel_size = 1
+
+if not torch.distributed.is_initialized():
+    torch.distributed.init_process_group(backend='gloo')
+
+# Initialize the model
+generator = Llama.build(
+    ckpt_dir=check_point_directory,
+    tokenizer_path=tokenizer_path,
+    max_seq_len=max_sequence_length,
+    max_batch_size=max_batch_size,
+    model_parallel_size=model_parallel_size
+)
+
+
+def dialog_chat_user(content: str):
+    dialogs_temp:[Dialog] = [ [{"role": "user", "content": content}]]
+    dialog_chat(dialogs_temp)
+
+
+def dialog_chat(dialogs_temp:[Dialog]):
+    results = generator.chat_completion(
+        dialogs_temp,
+        max_gen_len=max_sequence_length,
+        temperature=temperature,
+        top_p=top_p,
+    )
+    for dialog, result in zip(dialogs_temp, results):
+        for msg in dialog:
+            print(f"{msg['role'].capitalize()}: {msg['content']}\n")
+        print(
+            f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}"
+        )
+        print("\n==================================\n")
+
 
 def main():
-    check_point_directory = "Meta-Llama-3-8B-Instruct"
-    tokenizer_path = "Meta-Llama-3-8B-Instruct/tokenizer.model"
-    temperature = 0.6  # How much randomness is included in model output
-    top_p = 0.9  # Top-p probability threshold for nucleus sampling
-    max_sequence_length = 512  # The llama 3 model model produces up to 8192 tokens so max sequence length needs to be <= 8192
-    max_batch_size = 4
-    model_parallel_size = 1
-
-    if not torch.distributed.is_initialized():
-        torch.distributed.init_process_group(backend='gloo')
-
-    # Initialize the model
-    generator = Llama.build(
-        ckpt_dir=check_point_directory,
-        tokenizer_path=tokenizer_path,
-        max_seq_len=max_sequence_length,
-        max_batch_size=max_batch_size,
-        model_parallel_size=model_parallel_size
-    )
 
     dialogs: List[Dialog] = [
         [{"role": "user", "content": "what is the recipe of mayonnaise?"}],
